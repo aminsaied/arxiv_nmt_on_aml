3dbf54b279e45cd9bb61e73945a1c6c19aad50a5
Amin Saied <amsaied@microsoft.com>
2020-02-20 21:04:32 -0800

    create ingest pipeline step

---
+++ modules/ingest/ingest.py
@@ -0,0 +1,32 @@
+import argparse
+import datetime
+
+from arxiv_harvester import ArxivHarvester
+
+# Define arguments
+parser = argparse.ArgumentParser(description='arXiv scraping arg parser')
+parser.add_argument('--output_dir', type=str, help='Directory to store output raw data')
+parser.add_argument('--start_date', type=str, help='Start date for harvest')
+parser.add_argument('--end_date', type=str, help='End date for harvest')
+args = parser.parse_args()
+
+# Get arguments from parser
+output_dir = args.output_dir
+start_date = args.start_date
+end_date = args.end_date
+
+def validate(date_text):
+    try:
+        datetime.datetime.strptime(date_text, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Date should be in the form YYYY-MM-DD")
+
+if __name__ == '__main__':
+
+    # validate start and end date format
+    validate(start_date)
+    validate(end_date)
+
+    # harvest data from arXiv
+    harvester = ArxivHarvester(arxiv_data_path=output_dir)
+    harvester.harvest(start_date=start_date, end_date=end_date)
---
+++ modules/ingest/ingest_step.py
@@ -0,0 +1,62 @@
+import os
+from azureml.pipeline.steps import PythonScriptStep
+from azureml.core.runconfig import RunConfiguration
+from azureml.pipeline.core import PipelineData
+from azureml.pipeline.core import PipelineParameter
+
+def data_ingestion_step(datastore, compute_target):
+    '''
+    Collects metadata of all the papers published between two given days (both
+    days given as YYYY-MM-DD strings). The metadata we collect is:
+
+    -- 'arXiv_id' , unique id, of the format 'int.int' or just 'int'
+    -- 'Title' , the title of the paper
+    -- 'Authors' , as a string of last and first names separated by a delimiter
+    -- 'Date' , date of publication, YYYY-MM-DD
+    -- 'MSCs' , string of 5-character codes, separated by a delimiter.
+    -- 'Abstract' , short description of the content of the paper
+
+    :param datastore: The datastore that will be used
+    :type datastore: Datastore
+    :param compute_target: The compute target to run the step on
+    :type compute_target: ComputeTarget
+    
+    :return: The ingestion step, step outputs dictionary (keys: raw_data_dir)
+    :rtype: PythonScriptStep, dict
+    '''
+
+    run_config = RunConfiguration()
+    run_config.environment.environment_variables = {
+        'AZURE_REGION': datastore._workspace.location
+        }
+    run_config.environment.docker.enabled = True
+
+    start_date = PipelineParameter(name='start_date', default_value='2015-01-01')
+    end_date = PipelineParameter(name='end_date', default_value='2015-06-01')
+
+    raw_data_dir = PipelineData(
+        name='raw_data_dir', 
+        pipeline_output_name='raw_data_dir',
+        datastore=datastore,
+        output_mode='mount',
+        is_directory=True)
+
+    outputs = [raw_data_dir]
+    outputs_map = { 'raw_data_dir': raw_data_dir }
+
+    step = PythonScriptStep(
+        name="Ingestion",
+        script_name='ingestion.py',
+        arguments=[
+            '--output_dir', raw_data_dir, 
+            '--start_date', start_date, 
+            '--end_date', end_date
+            ],
+        outputs=outputs,
+        compute_target=compute_target,
+        source_directory=os.path.dirname(os.path.abspath(__file__)),
+        runconfig=run_config,
+        allow_reuse=True
+    )
+
+    return step, outputs_map
